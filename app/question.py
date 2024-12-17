cb59a4e2450a18e99885e26ef34917c36b4de800
thushan-123 <thushanmadusanka456@gmail.com>
2024-12-17 20:49:23 +0530

    question imple

--- app/question.py
+++ app/question.py
@@ -1,16 +1,21 @@
 class Question():
     
-    def __init__(self,bytearray):
-        val_ = bytearray[0] + bytearray[bytearray[0] + 1] + 3 # filter -> \x07example\x03com\x00 
-        print(val_)
+    def __init__(self,bytearray,qtype,qclass):
+        self.bytearray = bytearray
+        self.qtype = qtype
+        self.qclass = qclass
         
+    def flags_to_byte(self):
+        val_ = bytearray[0] + bytearray[bytearray[0] + 1] + 3   # Filter -> \x07example\x03com\x00 
+        print(val_)
         bytearray = bytearray[:val_]
-         # Convert to hex value and append  
+          
         print(bytearray)
         
-        hex_val_ = [f"{i:02x}" for i in bytearray]
+        hex_val_ = [f"{i:02x}" for i in bytearray]  # Convert to hex value and append 
         hex_str = " ".join(hex_val_)
-        return hex_str
+        
+        
             
             
 '''
@@ -18,5 +23,8 @@ class Question():
     filter -> \x07example\x03com\x00
     example.com    -> 07 65 78 61 6d 70 6c 65 03 63 6f 6d 00  terminate
 '''
+
+q = Question(b'\x07example\x03com\x00\x00\x01\x00\x01\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00')
+q.flags_to_byte
 
 
