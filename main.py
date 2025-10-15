cd8ed8bc3e9428c4f613b8bb7ec7b1c40384cae5
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-15 07:43:37 +0530

    fix issue reciv fn rplade reciveFrom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import socket
 
 
+def handle_client(udp_socket: socket.socket, buffer):
+    pass
+
 def main():
     udp_socket = socket.socket(
         socket.AF_INET,
@@ -9,9 +12,16 @@ def main():
 
     udp_socket.bind(("127.0.0.1", 2053)) # bind ip and port
 
+    print("DNS Server running on 127.0.0.1:2053")
+
     while True:
         try:
-            buffer , source = udp_socket.recv(1024) #buffer size 1024 bytes
+            buffer , addr = udp_socket.recvfrom(1024) #buffer size 1024 bytes
+
+            print(buffer)
+
+            res = b"hello"
+            udp_socket.sendto(res, addr)
         except Exception as e:
             print(f"Err : {e}")
 
