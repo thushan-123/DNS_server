4874429066056bd9c4b0a6738feb4428e2c949fa
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-15 07:12:03 +0530

    create main method

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
 
     while True:
         try:
-            pass
+            buffer , source = udp_socket.recv(1024) #buffer size 1024 bytes
         except Exception as e:
             print(f"Err : {e}")
+
+
+if __name__ == "__main__":
+    main()
